f29777a8a1ea41287d5438ff2c37ac39eeae2cf7
Naiel <109038805+naielv@users.noreply.github.com>
2024-09-08 20:46:25 +0200

    Create backup.py

---
+++ tasks/backup.py
@@ -0,0 +1,30 @@
+import requests
+import os
+from yaml import safe_loads
+import json
+def copy(src: str, dest: str):
+  os.system(f'rsync -Lau --partial --no-perms --info=progress2 "{src}" "{dest}"')
+  return "Ok"
+
+def notify(msg: str):
+  requests.get("http://192.168.0.3:1880/endpoint/notbk", params={
+      "msg": msg
+  })
+
+def backup_db(dest: str):
+  os.system(f'mysqldump --all-databases > "{dest}"')
+
+def runBackups(config):
+  for target in config["Copias de seguridad"]["Bases de datos"]:
+    print(f"::Backups:: Haciendo copia de seguridad de las bases de datos")
+    print(f"::Backups::   Destino: " + target["Destino"])
+    backup_db(target["Destino"])
+  for target in config["Copias de seguridad"]["Carpetas"]:
+    print(f"::Backups:: Haciendo copia de seguridad de las carpetas")
+    print(f"::Backups::   Origen: " + target["Origen"])
+    print(f"::Backups::   Destino: " + target["Destino"])
+    copy(target["Origen"], target["Destino"])
+  notify(f'He hecho la copia de seguridad semanal.')
+if __name__ == "__main__":
+  config = safe_loads("/mnt/storage/HomelabConfig.yml")
+  runBackups(config)
